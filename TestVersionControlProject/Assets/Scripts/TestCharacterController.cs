83a4c0ec8c553d414385fced89637986e759e58b
Christian Pamaong <pamaong@shernet.sheridancollege.ca>
2020-01-10 10:00:07 -0500

    Added character controller and game manager scripts

---
+++ TestVersionControlProject/Assets/Scripts/TestCharacterController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCharacterController : MonoBehaviour
+{
+    public string charName = "ZODIAC";
+
+    public int maxHealth = 1337;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(charName + " setup.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(charName + " got " + maxHealth + " HP");
+    }
+}
